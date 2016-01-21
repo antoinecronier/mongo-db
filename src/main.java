95618340344cc4fec0a013bea06b3faa7a9404fb
Antoine Cronier <antoine.cronier@tactfactory.com>
2016-01-21 11:29:55 +0100

    change bulk op

--- src/main.java
+++ src/main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.BulkWriteOperation;
 import com.mongodb.BulkWriteResult;
@@ -6,6 +8,7 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class main {
@@ -56,36 +59,52 @@ public class main {
 	
 	//Retrieve items from db
 	query = new BasicDBObject("i", new BasicDBObject("$gt", 20).append("$lte", 30));
+	ArrayList<BasicDBObject> collRetrieve = new ArrayList<BasicDBObject>();
 	cursor = coll.find(query);
 	try {
 	    while (cursor.hasNext()) {
-	        System.out.println(cursor.next());
+	    	collRetrieve.add((BasicDBObject) cursor.next());
 	    }
 	} finally {
 	    cursor.close();
 	}
 	
+	for (BasicDBObject basicDBObject : collRetrieve) {
+		System.out.println(basicDBObject.get("i"));
+	}
+	
 	//Retrieve max possible items in a period of time
-	coll.find().maxTime(1, SECONDS).count();
+	int items = coll.find().maxTime(1, SECONDS).count();
 	
 	//Run chained operation on db
-	BulkWriteOperation builder = coll.initializeOrderedBulkOperation();
-	builder.insert(new BasicDBObject("_id", 1));
-	builder.insert(new BasicDBObject("_id", 2));
-	builder.insert(new BasicDBObject("_id", 3));
+	/*BulkWriteOperation builder = coll.initializeOrderedBulkOperation();
+	builder.insert(new BasicDBObject("id", 1));
+	builder.insert(new BasicDBObject("id", 2));
+	builder.insert(new BasicDBObject("id", 3));*/
+	DBObject document1 = new BasicDBObject();
+	document1.put("id", "1");
+	DBObject document2 = new BasicDBObject();
+	document2.put("id", "2");    
+	DBObject document3 = new BasicDBObject();
+	document2.put("id", "3");
+	ArrayList<DBObject> documents = new ArrayList<DBObject>();
+	documents.add(document1);
+	documents.add(document2);
+	documents.add(document3);
+	coll.insert(documents);
 
-	builder.find(new BasicDBObject("_id", 1)).updateOne(new BasicDBObject("$set", new BasicDBObject("x", 2)));
-	builder.find(new BasicDBObject("_id", 2)).removeOne();
-	builder.find(new BasicDBObject("_id", 3)).replaceOne(new BasicDBObject("_id", 3).append("x", 4));
+	/*builder.find(new BasicDBObject("id", 1)).updateOne(new BasicDBObject("$set", new BasicDBObject("x", 2)));
+	builder.find(new BasicDBObject("id", 2)).removeOne();
+	builder.find(new BasicDBObject("id", 3)).replaceOne(new BasicDBObject("_id", 3).append("x", 4));*/
 
-	BulkWriteResult result = builder.execute();
+	//BulkWriteResult result = builder.execute();
 
 	// 2. Unordered bulk operation - no guarantee of order of operation
-	builder = coll.initializeUnorderedBulkOperation();
+	/*builder = coll.initializeUnorderedBulkOperation();
 	builder.find(new BasicDBObject("_id", 1)).removeOne();
 	builder.find(new BasicDBObject("_id", 2)).removeOne();
 
-	result = builder.execute();
+	result = builder.execute();*/
 	
 	}
 }
